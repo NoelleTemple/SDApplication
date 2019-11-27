e2e4ef62e49a27c288770463666112624914c3a2
NoelleTemple <tug48873@temple.edu>
2019-11-27 12:25:34 -0500

    adding ranges

--- app/src/main/java/com/example/myapplication/ProfileInfo.java
+++ app/src/main/java/com/example/myapplication/ProfileInfo.java
@@ -12,6 +12,28 @@ public class ProfileInfo implements Serializable {
         this.name = String.valueOf(name);
         this.sex = sex;
         this.age = age;
+        float[] ranges = new float[14][2];
+        ranges [0][0] =
+        ranges [0][1] = 65.5; //75+ 
+
+        if (age > 74){
+
+        } else if (age > 64 ){
+
+            } else if (age > 54){
+
+            } else if (age > 44){
+
+            } else if (age > 34){
+
+            } else if (age >24){
+
+            } else if (age>17){
+
+            } else{
+
+            }
+
     }
 
     public String getName()
