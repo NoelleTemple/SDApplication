a64192bf1e982ae864e3fa6bac3e816fe42c5c63
NoelleTemple <tug48873@temple.edu>
2019-11-27 15:30:42 -0500

    minor edit

--- app/src/main/java/com/example/myapplication/DrivingPage.java
+++ app/src/main/java/com/example/myapplication/DrivingPage.java
@@ -3,7 +3,6 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-//import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
