6fa1c37015c75c6b8aa2fc267485eacf0a54c37c
NoelleTemple <tug48873@temple.edu>
2019-11-27 15:30:02 -0500

    alarm added

--- app/src/main/java/com/example/myapplication/DrivingPage.java
+++ app/src/main/java/com/example/myapplication/DrivingPage.java
@@ -1,13 +1,19 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+//import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.io.IOException;
+
 public class DrivingPage extends AppCompatActivity {
-ProfileInfo profile;
+    ProfileInfo profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +28,28 @@ ProfileInfo profile;
                 goHome();
             }
         });
+
+        final boolean[] i = {false};
+
+        final MediaPlayer alarm = MediaPlayer.create(DrivingPage.this, R.raw.alarm);
+
+        Button alarm_button = findViewById(R.id.alarm_button);
+        alarm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i[0] = !i[0];
+                if (i[0]) {
+                    alarm.start();
+                    alarm.setLooping(true);
+                } else {
+                    alarm.pause();
+                    //alarm.stop(); //to completely stop
+                }
+            }
+        });
+
     }
+
     private void goHome() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
